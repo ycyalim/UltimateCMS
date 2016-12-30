34f113cf55e79654b17eabf290dd1948dedec7e3
ycyalim <ycyalim@hotmail.com>
2016-12-30 23:27:57 +0300

    Add project files.

---
+++ DataLayer/UltimateContext.cs
@@ -0,0 +1,16 @@
+﻿using DataModel;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace DataLayer
+{
+    public class UltimateContext : DbContext
+    {
+
+        public DbSet<HtmlData> HtmlDatas { get; set; }
+        public DbSet<Widget> Widget { get; set; }
+     }
+}
---
+++ UnitTestProject/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DataLayer;
+using DataModel;
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            UltimateContext ctx1 = new UltimateContext();
+
+            HtmlData data = new HtmlData();
+            data.Data = "<div>asdd asdsadas</div>";
+            ctx1.HtmlDatas.Add(data);
+            ctx1.SaveChanges();
+            Assert.IsNotNull(data.Id);
+        }
+    }
+}
